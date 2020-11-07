5176eed6fcbf12c6a5d5e5d915c75fbd273ab71d
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-11-07 17:34:47 +0100

    Creating event to display route

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { Button, Form, Container } from "semantic-ui-react";
 
 const Map = () => {
   const [currentPosition, setCurrentPosition] = useState({});
@@ -17,8 +18,21 @@ const Map = () => {
     navigator.geolocation.getCurrentPosition(success);
   });
 
+  const mapRoute = async (event) => {
+    event.preventDefault();
+
+    const destination = event.target.destination.value;
+
+  const onMarkerDragEnd = (e) => {
+    const lat = e.latLng.lat();
+    const lng = e.latLng.lng();
+    setCurrentPosition({ lat, lng})
+  };
+
+
+
   const mapStyles = {
-    height: "70vh",
+    height: "80vh",
     width: "100%",
   };
 
@@ -28,17 +42,40 @@ const Map = () => {
   // };
 
   return (
+    <Container >
     <div className="map" data-cy="map">
       <LoadScript googleMapsApiKey={apiKey}>
         <GoogleMap 
           mapContainerStyle={mapStyles}
           zoom={13}
-          center={currentPosition}
-        >
-          {currentPosition.lat && <Marker position={currentPosition} />}
+          center={currentPosition}>
+          {
+          currentPosition.lat ?
+          <Marker position={currentPosition} 
+          onDragEnd={(e) => onMarkerDragEnd(e)}
+          draggable={true}/> : null
+        }
         </GoogleMap>
       </LoadScript>
     </div>
+    <Form data-cy="map" onSubmit={(e) => mapRoute(e)}>
+        <Form.Input
+          label="Location"
+          placeholder="Type in your location"
+          name="origin"
+          type="input"
+          id="from"
+          data-cy="from"
+          required
+        />
+        </Form>
+        <Button
+          data-cy="submit"
+          id="submit-route"
+          content="Submit Your Route"
+          color="green"
+        ></Button>
+    </Container>
   );
 };
 export default Map;
